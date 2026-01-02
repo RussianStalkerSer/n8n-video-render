b653a5652c380c238ce2f23dc9c55999af8560d8
RussianStalkerSer <156858317+RussianStalkerSer@users.noreply.github.com>
2026-01-02 22:59:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -22,4 +22,5 @@ def render():
         return send_file(out_path, mimetype="video/mp4")
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
